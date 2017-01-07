5be78f33946f4a713c023157c13d039b16902e93
diegomcsilva <diegomcsilva191@gmail.com>
2017-01-07 01:15:47 -0200

    Includ Greetings

--- JavaScritp/main.js
+++
@@ -1,9 +0,0 @@
-// adding initial time classnames on load. css handles the timing.
-var time = new Date(),
-    hours = time.getHours(),
-    minutes = time.getMinutes(),
-    seconds = time.getSeconds();
-
-document.getElementById("second-hand").className = "sh-" + seconds;
-document.getElementById("minute-hand").className = "mh-" + minutes;
-document.getElementById("hour-hand").className = "hh-" + hours;
